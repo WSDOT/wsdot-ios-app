eecb510ed55cb2d7182b8a0ff740bc91d6a9670b
Logan Sims <simsl@wsdot.wa.gov>
2016-06-29 08:59:42 -0700

    Added source
    Simple home menu with navigation to ferries home

---
+++ wsdot/FerriesHomeViewController.swift
@@ -0,0 +1,54 @@
+//
+//  FerriesHomeTableViewController.swift
+//  wsdot
+//
+//  Created by Logan Sims on 6/29/16.
+//  Copyright © 2016 wsdot. All rights reserved.
+//
+
+import UIKit
+
+class FerriesHomeViewController: UITableViewController {
+
+    let cellIdentifier = "FerriesHomeCell"
+    var menu_options: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Set Title
+        title = "Ferries"
+        menu_options = ["Route Schedules", "Vehicle Reservations", "VesselWatch"]
+        tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: cellIdentifier)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: -
+    // MARK: Table View Data Source Methods
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menu_options.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        // Configure Cell
+        cell.textLabel?.text = menu_options[indexPath.row]
+     
+        return cell
+    }
+
+    // MARK: -
+    // MARK: Table View Delegate Methods
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(menu_options[indexPath.row])
+    }
+}
---
+++ wsdot/HomeViewController.swift
@@ -0,0 +1,64 @@
+//
+//  HomeViewController.swift
+//  wsdot
+//
+//  Created by Logan Sims on 6/29/16.
+//  Copyright © 2016 wsdot. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+
+    let cellIdentifier = "HomeCell"
+    let SegueFerriesHomeViewController = "FerriesHomeViewController"
+    
+    var menu_options: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Set Title
+        title = "WSDOT"
+        menu_options = ["Traffic Map", "Ferries", "Mountain Passes", "Social Media", "Toll Rates", "Border Waits", "Amtrak Cascades"]
+        tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: cellIdentifier)
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: -
+    // MARK: Table View Data Source Methods
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menu_options.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        // Configure Cell
+        cell.textLabel?.text = menu_options[indexPath.row]
+     
+        return cell
+    }
+
+    // MARK: -
+    // MARK: Table View Delegate Methods
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(menu_options[indexPath.row])
+        // Perform Segue
+        switch (indexPath.row) {
+            case 1:
+                performSegueWithIdentifier(SegueFerriesHomeViewController, sender: self)
+                tableView.deselectRowAtIndexPath(indexPath, animated: true)
+            default:
+                break
+        }
+    }
+}
