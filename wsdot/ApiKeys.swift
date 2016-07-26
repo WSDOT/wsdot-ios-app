e603e297a2c65d3687f6b8dcfa26ee75c1407863
Logan Sims <simsl@wsdot.wa.gov>
2016-07-26 11:02:01 -0700

    Added Api key file

---
+++ wsdot/ApiKeys.swift
@@ -0,0 +1,16 @@
+//
+//  ApiKeys.swift
+//  WSDOT
+//
+//  Created by Logan Sims on 7/26/16.
+//  Copyright © 2016 wsdot. All rights reserved.
+//
+
+import Foundation
+
+class ApiKeys {
+
+    static let wsdot_key = ""
+
+
+}
