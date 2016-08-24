c0d61822cfa94cea5047fd14f756c3e8025d3c68
Logan Sims <simsl@wsdot.wa.gov>
2016-08-24 08:50:51 -0700

    Added border wait files

---
+++ wsdot/BorderWaitItem.swift
@@ -0,0 +1,16 @@
+//
+//  BorderWaitItem.swift
+//  WSDOT
+//
+//  Created by Logan Sims on 8/24/16.
+//  Copyright © 2016 WSDOT. All rights reserved.
+//
+
+import RealmSwift
+
+class BorderWaitItem: Object {
+
+
+
+
+}
---
+++ wsdot/BorderWaitRealmStore.swift
@@ -0,0 +1,20 @@
+//
+//  BorderWaitRealmStore.swift
+//  WSDOT
+//
+//  Created by Logan Sims on 8/24/16.
+//  Copyright © 2016 WSDOT. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class BorderWaitStore{
+
+
+
+
+
+
+
+}
