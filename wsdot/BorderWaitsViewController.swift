e37b4ddc42f516a262235d031c76734b4463bd34
Logan Sims <simsl@wsdot.wa.gov>
2016-08-24 10:33:53 -0700

    Condensed Border waits into one ViewController
    Uses two different arrays for north and south bound waits.
    Segment control sets display array.

--- wsdot/BorderWaitsViewController.swift
+++ wsdot/BorderWaitsViewController.swift
@@ -8,16 +8,57 @@
 
 import UIKit
 
-class BorderWaitsViewController: UIViewController{
+class BorderWaitsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
-    @IBOutlet weak var southboundViewContrainer: UIView!
-    @IBOutlet weak var northboundViewContainer: UIView!
+    let cellIdentifier = "borderwaitcell"
+
+    @IBOutlet weak var tableView: UITableView!
+    var displayedWaits = [BorderWaitItem]()
+    var northboundWaits = [BorderWaitItem]()
+    var southboundWaits = [BorderWaitItem]()
     
     override func viewDidLoad() {
         title = "Border Waits"
         
-        southboundViewContrainer.hidden = true
-        northboundViewContainer.hidden = false
+        let testWait = BorderWaitItem()
+        
+        testWait.id = 0
+        testWait.name = "Lynden/Aldergrove"
+        testWait.route = 5
+        testWait.lane = "car"
+        testWait.waitTime = 10
+        testWait.updated = "2016-08-24 09:05 AM"
+    
+        northboundWaits.append(testWait)
+        
+        displayedWaits = northboundWaits
+    }
+
+    // MARK: Table View Data Source Methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return displayedWaits.count
+    }
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! BorderWaitCell
+        
+        let wait = displayedWaits[indexPath.row]
+
+        cell.nameLabel.text = wait.name
+        cell.updatedLabel.text = TimeUtils.timeAgoSinceDate(TimeUtils.formatTimeStamp(wait.updated), numericDates: false)
+        cell.waitTimeLabel.text = String(wait.waitTime) + " min"
+        cell.RouteImage.image = UIImage(named: "icListI5")
+                
+        return cell
     }
     
     // Remove and add hairline for nav bar
@@ -35,12 +76,12 @@ class BorderWaitsViewController: UIViewController{
     @IBAction func indexChanged(sender: UISegmentedControl) {
         switch (sender.selectedSegmentIndex){
         case 0:
-            southboundViewContrainer.hidden = true
-            northboundViewContainer.hidden = false
+            displayedWaits = northboundWaits
+            tableView.reloadData()
             break
         case 1:
-            southboundViewContrainer.hidden = false
-            northboundViewContainer.hidden = true
+            displayedWaits = southboundWaits
+            tableView.reloadData()
             break
         default:
             break
