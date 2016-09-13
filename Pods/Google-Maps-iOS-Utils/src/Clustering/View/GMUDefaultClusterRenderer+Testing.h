5622d406c3f23588ced12246930e2a0a5af87f99
Logan Sims <simsl@wsdot.wa.gov>
2016-09-13 10:24:28 -0700

    removed unused pod library

--- Pods/Google-Maps-iOS-Utils/src/Clustering/View/GMUDefaultClusterRenderer+Testing.h
+++
@@ -1,9 +0,0 @@
-#import "GMUDefaultClusterRenderer.h"
-
-/* Extensions for testing purposes only. */
-@interface GMUDefaultClusterRenderer (Testing)
-
-// Returns currently active markers.
-- (NSArray<GMSMarker *> *)markers;
-
-@end
