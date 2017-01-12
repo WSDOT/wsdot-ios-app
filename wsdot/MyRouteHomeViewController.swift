26a8bb1f1b8e9f51f7e306d7c8f6d993bcde6f3f
Logan Sims <simsl@wsdot.wa.gov>
2017-01-12 08:37:31 -0800

    WIP: My Route

---
+++ wsdot/MyRouteHomeViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MyRouteHomeViewController.swift
+//  WSDOT
+//
+//  Copyright (c) 2017 Washington State Department of Transportation
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>
+//
+
+import UIKit
+
+class MyRouteHomeViewController: UIViewController {
+
+
+    let segueNewMyRouteViewController = "NewMyRouteViewController"
+    
+    @IBOutlet weak var WelcomeView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+    @IBAction func createRouteButtonPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: segueNewMyRouteViewController, sender: self)
+    }
+
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
