WSDOT/wsdot-ios-app

commit 204cfa11f67863cdadaa0231b506d95c4f511242

Fixes issue with GoTo location logic
Issue was caused by changes to the MapViewController for Push Alerts.
MapVC now checks user defaults in willAppear and overrode the setting
from goto.

wsdot/TrafficMapGoToViewController.swift
@@ -83,7 +83,103 @@ class TrafficMapGoToViewController: UIViewController, UITableViewDataSource, UIT
     // MARK: Table View Delegate Methods
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         GoogleAnalytics.screenView(screenName: "/Traffic Map/GoTo Location/" + menu_options[indexPath.row])
+        setGoToLocation(index: indexPath.row)
         self.dismiss(animated: true, completion: {()->Void in});
-        my_parent?.goTo(indexPath.row)
+    }
+    
+    func setGoToLocation(index: Int) {
+        var lat = 0.0
+        var long = 0.0
+        var zoom = 0
+        
+        switch(index) {
+        case 0:
+            lat = 48.756302
+            long = -122.46151
+            zoom = 12 // Bellingham
+            break
+        case 1:
+            lat = 46.635529
+            long = -122.937698
+            zoom = 11 // Chehalis
+            break
+        case 2:
+            lat = 47.85268
+            long = -122.628365
+            zoom = 13 // Hood Canal
+            break
+        case 3:
+            lat = 47.859476
+            long = -121.972446
+            zoom = 13 // Monroe
+            break
+        case 4:
+            lat = 48.420657
+            long = -122.334824
+            zoom = 13 // Mt Vernon
+            break
+        case 5:
+            lat = 47.021461
+            long = -122.899933
+            zoom = 13 // Olympia
+            break
+        case 6:
+            lat = 47.5990
+            long = -122.3350
+            zoom = 12 // Seattle
+            break
+        case 7:
+            lat = 47.404481
+            long = -121.4232569
+            zoom = 12 // Snoqualmie Pass
+            break
+        case 8:
+            lat = 47.658566
+            long = -117.425995
+            zoom = 12 // Spokane
+            break
+        case 9:
+            lat = 48.22959
+            long = -122.34581
+            zoom = 13 // Stanwood
+            break
+        case 10:
+            lat = 47.86034
+            long = -121.812286
+            zoom = 13 // Sultan
+            break
+        case 11:
+            lat = 47.206275
+            long = -122.46254
+            zoom = 12 // Tacoma
+            break
+        case 12:
+            lat = 46.2503607
+            long = -119.2063781
+            zoom = 11 // Tri-Cities
+            break
+        case 13:
+            lat = 45.639968
+            long = -122.610512
+            zoom = 11 // Vancouver
+            break
+        case 14:
+            lat = 47.435867
+            long = -120.309563
+            zoom = 12 // Wenatchee
+            break
+        case 15:
+            lat = 46.6063273
+            long = -120.4886952
+            zoom = 11 // Takima
+            break
+        default:
+            break
+        }
+        
+        UserDefaults.standard.set(lat, forKey: UserDefaultsKeys.mapLat)
+        UserDefaults.standard.set(long, forKey: UserDefaultsKeys.mapLon)
+        UserDefaults.standard.set(zoom, forKey: UserDefaultsKeys.mapZoom)
+    
     }
 }

wsdot/TrafficMapViewController.swift
@@ -187,64 +187,7 @@ class TrafficMapViewController: UIViewController, MapMarkerDelegate, GMSMapViewD
             mapView.moveCamera(GMSCameraUpdate.setCamera(camera))
         }
     }
-    
-    func goTo(_ index: Int){
-        if let mapView = embeddedMapViewController.view as? GMSMapView{
-            switch(index){
-            case 0:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 48.756302, longitude: -122.46151, zoom: 12))) // Bellingham
-                break
-            case 1:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 46.635529, longitude: -122.937698, zoom: 11))) // Chehalis
-                break
-            case 2:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.85268, longitude: -122.628365, zoom: 13))) // Hood Canal
-                break
-            case 3:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.859476, longitude: -121.972446, zoom: 13))) // Monroe
-                break
-            case 4:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 48.420657, longitude: -122.334824, zoom: 13))) // Mt Vernon
-                break
-            case 5:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.021461, longitude: -122.899933, zoom: 13))) // Olympia
-                break
-            case 6:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.5990, longitude: -122.3350, zoom: 12))) // Seattle
-                break
-            case 7:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.404481, longitude: -121.4232569, zoom: 12))) // Snoqualmie Pass
-                break
-            case 8:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.658566, longitude: -117.425995, zoom: 12))) // Spokane
-                break
-            case 9:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 48.22959, longitude: -122.34581, zoom: 13))) // Stanwood
-                break
-            case 10:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.86034, longitude: -121.812286, zoom: 13))) // Sultan
-                break
-            case 11:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.206275, longitude: -122.46254, zoom: 12))) // Tacoma
-                break
-            case 12:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 46.2503607, longitude: -119.2063781, zoom: 11))) // Tri-Cities
-                break
-            case 13:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 45.639968, longitude: -122.610512, zoom: 11))) // Vancouver
-                break
-            case 14:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 47.435867, longitude: -120.309563, zoom: 12))) // Wenatchee
-                break
-            case 15:
-                mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: 46.6063273, longitude: -120.4886952, zoom: 11))) // Takima
-                break
-            default:
-                break
-            }
-        }
-    }
-    
+
     /*
         Checks if "best times to travel charts" are available from the data server,
         if they are, display an alert badge on the Traveler information menu

wsdot/AppDelegate.swift
@@ -35,7 +35,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     
         let theme = Theme(rawValue: EventStore.getActiveEventThemeId()) ?? .defaultTheme
         ThemeManager.applyTheme(theme: theme)
-
         migrateRealm()
         CachesStore.initCacheItem()
         
@@ -185,7 +184,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
         let ferrySailings = mainStoryboard.instantiateViewController(withIdentifier: "RouteTabBarViewController") as! RouteTabBarViewController
   
         // set values for the sailings VC
-        // TODO: what if there are no alerts? (not sure if this is already handled...)
         ferrySailings.routeId = routeId
         ferrySailings.selectedTab = 1
         

wsdot/Base.lproj/Main.storyboard
@@ -245,6 +245,7 @@
                         </rightBarButtonItems>
                     </navigationItem>
                     <connections>
+                        <outlet property="notificationBarButton" destination="sPu-kK-o5w" id="NLU-5V-kDt"/>
                         <outlet property="tableView" destination="peh-Jp-BEr" id="o2R-WZ-ota"/>
                         <segue destination="YHC-Qx-tw0" kind="showDetail" identifier="InfoViewController" id="nAK-Qt-76l"/>
                         <segue destination="V16-il-1em" kind="showDetail" identifier="TrafficMapViewController" id="gN6-Dz-E6K"/>

wsdot/BestTimesToTravelDetailsViewController.swift
@@ -43,7 +43,7 @@ class BestTimesToTravelDetailsViewController: UIViewController, UITableViewDataS
     // MARK: -
     // MARK: Table View Data source methods
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1 // TODO Make groups for each route
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {

wsdot/FerryRealmStore.swift
@@ -69,7 +69,8 @@ class FerryRealmStore {
         
             if ((delta > TimeUtils.updateTime) || force){
             
-                Alamofire.request("http://data.wsdot.wa.gov/mobile/WSFRouteSchedules.js").validate().responseJSON { response in
+            
+                Alamofire.request("http://data.wsdot.wa.gov/mobile/ferryTEST.js").validate().responseJSON { response in
                     switch response.result {
                     case .success:
                         if let value = response.result.value {

wsdot/Info.plist
@@ -5,7 +5,7 @@
 	<key>CFBundleDevelopmentRegion</key>
 	<string>en_US</string>
 	<key>CFBundleDisplayName</key>
-	<string></string>
+	<string>$(PRODUCT_NAME)</string>
 	<key>CFBundleExecutable</key>
 	<string>$(EXECUTABLE_NAME)</string>
 	<key>CFBundleIdentifier</key>

wsdot/MapViewController.swift
@@ -102,9 +102,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMUCluster
     }
     
     override func viewWillAppear(_ animated: Bool) {
-    
-        print("view will appear")
-    
+
         var lat = UserDefaults.standard.double(forKey: UserDefaultsKeys.mapLat)
         var lon = UserDefaults.standard.double(forKey: UserDefaultsKeys.mapLon)
         var zoom = UserDefaults.standard.float(forKey: UserDefaultsKeys.mapZoom)
@@ -123,8 +121,6 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMUCluster
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        
-        print("view will disappear")
         
         if let mapView = view as? GMSMapView{
             UserDefaults.standard.set(mapView.camera.target.latitude, forKey: UserDefaultsKeys.mapLat)

commit 9a49b3b0cab530569425b4306948faa2466820e7

Fixes go to location issue, adds Everett to list
Adds Everett to the GoTo Locations list.

Fixes issue introduced in commit 204cfa11 where the go to locations
menu fails to work on iPad or devices that display the menu as a pop up.

The menu had previously only set the location defaults, which are never
read read when the menu is a pop up.

wsdot/TrafficMapGoToViewController.swift
@@ -33,6 +33,7 @@ class TrafficMapGoToViewController: UIViewController, UITableViewDataSource, UIT
         
         menu_options = ["Bellingham",
                         "Chehalis",
+                        "Everett",
                         "Hood Canal",
                         "Monroe",
                         "Mt Vernon",
@@ -90,7 +91,7 @@ class TrafficMapGoToViewController: UIViewController, UITableViewDataSource, UIT
     func setGoToLocation(index: Int) {
         var lat = 0.0
         var long = 0.0
-        var zoom = 0
+        var zoom: Float = 0
         
         switch(index) {
         case 0:
@@ -104,71 +105,76 @@ class TrafficMapGoToViewController: UIViewController, UITableViewDataSource, UIT
             zoom = 11 // Chehalis
             break
         case 2:
+            lat = 47.967976
+            long = -122.197627
+            zoom = 12
+            break
+        case 3:
             lat = 47.85268
             long = -122.628365
             zoom = 13 // Hood Canal
             break
-        case 3:
+        case 4:
             lat = 47.859476
             long = -121.972446
             zoom = 13 // Monroe
             break
-        case 4:
+        case 5:
             lat = 48.420657
             long = -122.334824
             zoom = 13 // Mt Vernon
             break
-        case 5:
+        case 6:
             lat = 47.021461
             long = -122.899933
             zoom = 13 // Olympia
             break
-        case 6:
+        case 7:
             lat = 47.5990
             long = -122.3350
             zoom = 12 // Seattle
             break
-        case 7:
+        case 8:
             lat = 47.404481
             long = -121.4232569
             zoom = 12 // Snoqualmie Pass
             break
-        case 8:
+        case 9:
             lat = 47.658566
             long = -117.425995
             zoom = 12 // Spokane
             break
-        case 9:
+        case 10:
             lat = 48.22959
             long = -122.34581
             zoom = 13 // Stanwood
             break
-        case 10:
+        case 11:
             lat = 47.86034
             long = -121.812286
             zoom = 13 // Sultan
             break
-        case 11:
+        case 12:
             lat = 47.206275
             long = -122.46254
             zoom = 12 // Tacoma
             break
-        case 12:
+        case 13:
             lat = 46.2503607
             long = -119.2063781
             zoom = 11 // Tri-Cities
             break
-        case 13:
+        case 14:
             lat = 45.639968
             long = -122.610512
             zoom = 11 // Vancouver
             break
-        case 14:
+        case 15:
             lat = 47.435867
             long = -120.309563
             zoom = 12 // Wenatchee
             break
-        case 15:
+        case 16:
             lat = 46.6063273
             long = -120.4886952
             zoom = 11 // Takima
@@ -180,6 +186,8 @@ class TrafficMapGoToViewController: UIViewController, UITableViewDataSource, UIT
         UserDefaults.standard.set(lat, forKey: UserDefaultsKeys.mapLat)
         UserDefaults.standard.set(long, forKey: UserDefaultsKeys.mapLon)
         UserDefaults.standard.set(zoom, forKey: UserDefaultsKeys.mapZoom)
+    
+        my_parent?.goTo(lat, long, zoom)
     
     }
 }

wsdot/TrafficMapViewController.swift
@@ -189,7 +189,13 @@ class TrafficMapViewController: UIViewController, MapMarkerDelegate, GMSMapViewD
             mapView.moveCamera(GMSCameraUpdate.setCamera(camera))
         }
     }
-
+    
+    func goTo(_ lat: Double, _ long: Double, _ zoom: Float){
+        if let mapView = embeddedMapViewController.view as? GMSMapView{
+            mapView.moveCamera(GMSCameraUpdate.setCamera(GMSCameraPosition.camera(withLatitude: lat, longitude: long, zoom: zoom)))
+        }
+    }
+        
     /*
         Checks if "best times to travel charts" are available from the data server,
         if they are, display an alert badge on the Traveler information menu