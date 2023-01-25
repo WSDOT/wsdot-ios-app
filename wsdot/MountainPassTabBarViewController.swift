5d2683e9282ad3da6c412de0d62308b8b0ec3223
Andrew Englehorn <engleha@wsdot.wa.gov>
2023-01-25 11:04:05 -0800

    Add mountain pass name to navbar title

--- wsdot/MountainPassTabBarViewController.swift
+++ wsdot/MountainPassTabBarViewController.swift
@@ -30,7 +30,7 @@ class MountainPassTabBarViewController: UITabBarController{
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = "Mountain Passes"
+        title = passItem.name
         
         if (passItem.cameraIds.count == 0){
             self.tabBar.items?[0].title = "Report"
