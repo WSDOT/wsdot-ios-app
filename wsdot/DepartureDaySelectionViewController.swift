48aa3795dfcbbabae97a8266e30df0c4d45a9f65
Andrew Englehorn <engleha@wsdot.wa.gov>
2023-04-13 08:19:09 -0700

    Edited ferry schedule calendar alert message

--- wsdot/DepartureDaySelectionViewController.swift
+++ wsdot/DepartureDaySelectionViewController.swift
@@ -26,7 +26,7 @@ class DepartureDaySelectionViewController: UIViewController, UITableViewDelegate
     @IBAction func infoButton(_ sender: Any) {
            
            // Ferry Schedule Calendar Message
-           let alert = UIAlertController(title: "Ferry Schedule Calendar", message: "Future ferry schedules are provided for planning purposes and can change daily. Please monitor ferry alerts to stay notified of changes to your route. For additional trip planning information, visit the Washington State Ferries website.", preferredStyle: .alert);
+           let alert = UIAlertController(title: "Ferry Schedule Calendar", message: "Future ferry schedules are provided for planning purposes and can change daily. Please monitor ferry alerts to stay notified of changes to your route. For additional trip planning information visit the Washington State Ferries website.", preferredStyle: .alert);
 
            alert.addAction(UIAlertAction(title: "Close", style: .default, handler: nil));
            let action: UIAlertAction = UIAlertAction(title: "Ferry Website", style: .default, handler: {
