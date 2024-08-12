efa1da7ff103d804580a7654bf6a02d03041ec5c
Andrew Englehorn <andrew.englehorn@wsdot.wa.gov>
2024-08-12 16:48:13 -0700

    Decrease static tolls data cache time
    Cache time is now set to one hour.

--- wsdot/CachesStore.swift
+++ wsdot/CachesStore.swift
@@ -37,7 +37,7 @@ enum CachedData {
 class CachesStore {
     
     static let tollUpdateTime: Int = 60 // once a minute
-    static let staticTollUpdateTime: Int = 86400 // once a day
+    static let staticTollUpdateTime: Int = 3600 // once an hour
     
     static let updateTime: Int = 900 // once every 15 minutes
 
